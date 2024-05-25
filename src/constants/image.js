6cc600353abc70b750a7c0b6b0f965c3dc7047b0
ramoncodevale <ramonvalentim88@gmail.com>
2024-05-25 16:37:17 -0300

    add new project

--- src/constants/image.js
+++ src/constants/image.js
@@ -1,17 +1,23 @@
 import Projeto1 from '../assets/pexels-photo.png';
 import Projeto2 from '../assets/dashboard-nlw.png';
+import Projeto3 from '../assets/quiz-expert.png';
 import { FaReact } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io5';
 import { BiLogoTypescript } from "react-icons/bi";
 import { SiTailwindcss } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
 
 export const images = {
     Projeto1,
     Projeto2,
+    Projeto3,
     FaReact,
     IoLogoJavascript,
     BiLogoTypescript,
     SiTailwindcss,
-    
+    FaHtml5,
+    FaCss3Alt,
+
 
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -17,7 +17,16 @@ export const projects = [{
     description: "Um dashboard desenvolvido durante a Next Level Week da Rocketseat, que permite a visualização e gestão de dados dos participantes de forma dinâmica e estilizada.",
     imageUrl: images.Projeto2,
     technologies: [images.FaReact, images.BiLogoTypescript, images.SiTailwindcss],
-    projectLink: "https://dashboard-nlw-sd9e.vercel.app/" 
-     
+    projectLink: "https://dashboard-nlw-sd9e.vercel.app/"
   },
+
+  {
+    id: 3,
+    title: 'Quiz Expert',
+    description: "Rockseat Quiz Expert é um projeto dinâmico e interativo desenvolvido com o objetivo de aprimorar habilidades de aprendizado através de quizzes desafiadores. Com uma vasta gama de temas e perguntas criteriosamente selecionadas, este aplicativo proporciona uma experiência educativa envolvente para usuários de todos os níveis.",
+    imageUrl: images.Projeto3,
+    technologies: [images.FaHtml5, images.FaCss3Alt, images.IoLogoJavascript],
+    projectLink: "https://ramoncodevale.github.io/quiz-expert/"
+  },
+
 ]
